a584bb841d1456ef0310299f5b331f8e3365373a
Robert <z93hpjlr@duck.com>
2024-01-23 18:20:35 +0100

    Kapitel 0 Tests

--- game/kapitel0.c
+++ game/kapitel0.c
@@ -7,13 +7,12 @@ printf("Unbekannte Stimme: \"Wach auch.\"\n");
 printf("1. Versuche dich auf die mysteriöse Stimme zu konzentrieren\n2. Gehe zurück in den endlosen und friedlichen Zustand der Unwissenheit\n");
 scanf("%d", &choice);
 if (choice == 1){
-    printf()
+    printf("platzhalter");
 }
 else if (choice == 2){
     printf("Du merkst wie dich die Müdigkeit wieder zurück in die Dunkelheit zieht.\nDu willst nichts mehr als einfach nur zu schlafen\n");
-    printf("1. Gib auf und lass dich von der Dunkelheit konsumieren\n2. Greife den letzten Strang der dich noch zurück zur Realität bringen kann.\n")
-    scanf("%d", %choice);
-
+    printf("1. Gib auf und lass dich von der Dunkelheit konsumieren\n2. Greife den letzten Strang der dich noch zurück zur Realität bringen kann.\n");
+    scanf("%d", &choice);
 }
 return 0;
 }
