fb7a4b46c0f37d59e5b81b5dafa4f86067418d19
Robert <z93hpjlr@duck.com>
2024-01-23 11:44:33 +0100

    Update Script

--- relevante scripts/commitall.sh
+++ relevante scripts/commitall.sh
@@ -1,7 +1,11 @@
-#! /bin/bash
+#!/bin/bash
+if [ $(pwd) != "/Users/robber/Documents/DHBW/Programmieren/c-projekt" ]; then
+echo "Wrong directory man"
+exit 1
+fi
 git add -A
 echo "What should the commit message be:"
 read a
 git commit -m "${a}"
-git push --force
+git push
 
