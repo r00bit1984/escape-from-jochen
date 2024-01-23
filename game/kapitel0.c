c9e959f588debe2da191eec9da60293323d1b527
r00bit1984 <100493181+r00bit1984@users.noreply.github.com>
2024-01-23 18:16:00 +0100

    Kapitel 0 Idee

--- game/kapitel0.c
+++ game/kapitel0.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
+
 int main(){
-printf("
+int choice;
+printf("Alles ist dunkel. Dein Schädel brummt und du weißt nicht wo du gerade bist.\n");
+printf("Unbekannte Stimme: \"Wach auch.\"\n");
+printf("1. Versuche dich auf die mysteriöse Stimme zu konzentrieren\n2. Gehe zurück in den endlosen und friedlichen Zustand der Unwissenheit\n");
+scanf("%d", &choice);
+if (choice == 1){
+    printf()
+}
+elif (choice == 2){
+    printf("Du merkst wie dich die Müdigkeit wieder zurück in die Dunkelheit zieht.\nDu willst nichts mehr als einfach nur zu schlafen\n");
+    printf("1. Gib auf und lass dich von der Dunkelheit konsumieren\n2. Greife den letzten Strang der dich noch zurück zur Realität bringen kann.\n")
+    scanf("%d", %choice);
+
+}
 return 0;
 }
