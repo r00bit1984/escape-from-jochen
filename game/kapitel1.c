cdfe9c602e8af85059e9caf5b874072efc736271
r00bit1984 <z93hpjlr@duck.com>
2024-01-30 10:44:45 +0100

    Neue Optionen in Kapitel 1

--- game/kapitel1.c
+++ game/kapitel1.c
@@ -11,8 +11,8 @@
 bool getInput1();
 bool parseExecute1(char *input, char *progressionInit);
 
-static char input[100];
-static char progressionInit[5] = "0";
+static char input[20];
+static char progressionInit[20] = "0";
 
 int main1()
 {
@@ -48,6 +48,7 @@ bool parseExecute1(char *input, char *progressionInit)
             exit(1);
         }
         // Keine Progression
+        // INSPECT
         if (strcmp(choice, "lookaround") == 0 && strcmp(progression, "0") == 0)
         {
             printf("Das Zimmer hat keine Fenster und es ist sehr dunkel.\nDeine Sicht ist immer noch verschwommen und du musst dich anstrengen mehr als nur Silhoutten von Objekten um dich herum zu erkennen.\nIn dem Zimmer befindet sich ein altes "); 
@@ -72,11 +73,11 @@ bool parseExecute1(char *input, char *progressionInit)
             printf(RESET);
             printf("Du fragst dich warum du überhaupt hier bist.");
         }
-        else if (strcmp(choice, "inspect Bett") == 0)
+        else if (strcmp(choice, "inspect.Bett") == 0)
         {
             printf("Das Bett ist verstaubt, sieht jedoch trotzdem wie frisch gemacht aus.\nDas Bett erinnert dich an deine Müdigkeit und du würdest dich am liebsten einfach wieder schlafen legen.\n");
         }
-        else if (strcmp(choice, "inspect Schreibtisch") == 0)
+        else if (strcmp(choice, "inspect.Schreibtisch") == 0)
         {
             printf("Der Schreibtisch sieht uralt aus.\nEr hat eine "); 
             printf(BLU);
@@ -92,7 +93,46 @@ bool parseExecute1(char *input, char *progressionInit)
             printf(RESET);
             printf("auf der Schreibfläche.");
         }
+        else if (strcmp(choice, "inspect.Kleiderschrank") == 0)
+        {
+            printf("Der Kleiderschrank ist aus massivem, dunklem Holz.\nVielleicht lässt er sich öffnen?");
+        }
+        else if (strcmp(choice, "inspect.Fenster") == 0)
+        {
+            printf("Das Fenster sieht aus als wäre es schon immer verbarrikadiert gewesen.\nDu zweifelst daran die Holzplanken entfernen zu können.");
+        }
+        else if (strcmp(choice, "inspect.Bild") == 0)
+        {
+            printf("Das Bild hat einen vergoldeten Rahmen und bildet ein Portät eines Mannes ab.\nDer Mann hat eine aufrechte Position und hat eine stolze Ausstrahlung.\nDu fragst dich wer dieser Mann ist.");
+        }
         
+        // GOTO
+        else if (strcmp(choice, "goto.Bett") == 0)
+        {
+            printf("Du befindest dich nun vor dem Bett.\nHier gibt es nichts mehr für dich zu tun.");
+            strcpy(progression, "bett");
+        }
+        else if (strcmp(choice, "goto.Schreibtisch") == 0)
+        {
+            printf("Du befindest dich jetzt vor dem hölzernen Schreibtisch.");
+            strcpy(progression, "tisch");
+        }
+        else if (strcmp(choice, "goto.Kleiderschrank") == 0)
+        {
+            printf("Du befindest dich jetzt vor dem großen Kleiderschrank.");
+            strcpy(progression, "schrank");
+        }
+        else if (strcmp(choice, "goto.Fenster") == 0)
+        {
+            printf("Du befindest dich jetzt vor dem Fenster.\nDie Holzplanken wirst du nicht entfernen können.");
+            strcpy(progression, "fenster");
+        }
+        else if (strcmp(choice, "goto.Bild") == 0)
+        {
+            printf("Du befindest dich jetzt vor dem Gemälde.\nDer Mann hat eine ominöse Ausstrahlung.");
+            strcpy(progression, "fenster");
+        }
+
     }
 return true;
 }
